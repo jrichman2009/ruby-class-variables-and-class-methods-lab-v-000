58ab1f503211fdbd3a3694437e4b2023afa85638
Jeffrey Richman <jrichman2009@gmail.com>
2018-08-28 15:54:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,8 +12,8 @@ class Song
     @artist = artist
     @genre = genre
     @@count = @@count += 1
-    @@genres << @genre
-    @@artists << @artist
+    @@genres << genre
+    @@artists << artist
 
   end
 
